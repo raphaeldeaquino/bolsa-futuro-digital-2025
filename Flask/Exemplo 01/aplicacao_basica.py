50490c0f40f88d29294d56e21b1d73493f38b44e
Raphael Gomes <raphael.gomes@ifg.edu.br>
2025-10-29 21:02:54 -0300

    Adicionado conteúdo de Flask

---
+++ Flask/Exemplo 01/aplicacao_basica.py
@@ -0,0 +1,36 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return """
+    <html>
+    <head><title>Bolsa Futuro Digital</title></head>
+    <body>
+    <h1>Backend com Python</h1>
+    <p>Esta é a página Web do curso</p>
+    <p>Os slides estão disponível <a href="slides">aqui</a>.</p>
+    </body>
+    </html>
+    """
+
+
+@app.route('/slides')
+def slides():
+    return """
+    <html>
+    <head><title>Bolsa Futuro Digital</title></head>
+    <body>
+    <h1>Slides</h1>
+    <ul>
+    <li>Introdução</li>
+    <li>O que é Software?</li>
+    </ul>
+    <p>Voltar para <a href="/">home</a>.</p>
+    </body>
+    </html>
+    """
+
+if __name__ == '__main__':
+    app.run()
